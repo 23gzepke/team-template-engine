c0dfd7455527af52d507850106e20858ce778ce5
Grant Zepke <grantzepke@gmail.com>
2020-09-01 15:52:37 -0400

    updated intern manager class

--- develop/lib/Intern.js
+++ develop/lib/Intern.js
@@ -1,1 +1,34 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+//Write code to define and export the Intern class.
+const Employee = require("./Employee");
+const inquirer = require("inquirer");
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+      super(name, id, email);
+      this.school = school;
+    }
+
+    getSchool() {
+        return this.school;
+    }
+
+    getRole(){
+        return "Intern";
+    }
+
+    async askFollowUp(){
+
+        // question
+        const question = [
+            {
+            type: "input",
+            message: "Where does the Employee go to school?",
+            name: "school"
+            }
+        ]    
+
+        const data = await inquirer.prompt(question);
+        this.school = data.school;
+    }
+}
+
+module.exports = Intern;
--- develop/lib/Manager.js
+++ develop/lib/Manager.js
@@ -1,1 +1,34 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+// TODO: Write code to define and export the Manager class.
+const Employee = require("./Employee");
+const inquirer = require("inquirer");
+class Manager extends Employee {
+    constructor(name, id, email, officeNum) {
+      super(name, id, email);
+      this.officeNumber = officeNum;
+    }
+
+    getOfficeNumber() {
+        return this.officeNumber;
+    }
+
+    getRole(){
+        return "Manager";
+    }
+
+    async askFollowUp(){
+
+        // question
+        const question = [
+            {
+            type: "input",
+            message: "What's the Employee's office number?",
+            name: "officenum"
+            }
+        ]    
+
+        const data = await inquirer.prompt(question);
+        this.officeNum = data.officenum;
+    }
+}
+
+module.exports = Manager;
