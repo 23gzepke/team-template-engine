a04ea2232ad3fc32f3e26bf448e932327154c22b
Grant Zepke <grantzepke@gmail.com>
2020-09-01 15:47:58 -0400

    updated employee engineer class

--- develop/lib/Engineer.js
+++ develop/lib/Engineer.js
@@ -1,1 +1,34 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+const inquirer = require("inquirer");
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+      super(name, id, email);
+      this.github = github;
+    }
+
+    getGithub() {
+        return this.github;
+    }
+
+    getRole(){
+        return "Engineer";
+    }
+
+    async askFollowUp(){
+
+        // question
+        const question = [
+            {
+            type: "input",
+            message: "What's the Employee's GitHub?",
+            name: "github"
+            }
+        ]    
+
+        const data = await inquirer.prompt(question);
+        this.github = data.github;
+    }
+}
+
+module.exports = Engineer;
